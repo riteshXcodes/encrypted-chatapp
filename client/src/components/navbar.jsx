343c54b3e17979fe0dc27d6be764ba8845e49dae
riteshXcodes <riteshanand5555@gmail.com>
2025-07-12 04:09:02 +0530

    change frontend local host url to deployed url

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -5,13 +5,13 @@ const Navbar = () => {
     <div className='flex justify-between items-center p-4 bg-gray-800 text-white'>
      <nav className="flex justify-between items-center w-full max-w-6xl mx-auto">
         <ul className="flex space-x-15">
-            <li><a href='http://localhost:5173/'>Home</a></li>
+            <li><a href='https://encrypted-chatapp.vercel.app/'>Home</a></li>
             <li>Features</li>
             <li>About</li>
             <li>Contact Us</li>
         </ul>
         <ul className="flex space-x-6">
-            <li><div  className="flex items-center px-8 py-3 rounded-full border border-gray-900 bg-[#f9f4ec] text-gray-900 text-lg font-normal hover:bg-gray-100 transition"><a href='http://localhost:5173/login'>Login/Register</a>
+            <li><div  className="flex items-center px-8 py-3 rounded-full border border-gray-900 bg-[#f9f4ec] text-gray-900 text-lg font-normal hover:bg-gray-100 transition"><a href='https://encrypted-chatapp.vercel.app/login'>Login/Register</a>
               <span className="ml-2 text-xl">&#8594;</span></div></li>
         </ul>
         </nav> 
--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const http = require("http");
 const { Server } = require("socket.io");
 const Message = require("./models/message");
 const onlineUsers = new Map();
-const path = require("path");
 
 require("dotenv").config();
 
@@ -15,7 +14,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
-app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.get("/", (req, res) => {
   res.send("Server is up and running!");
@@ -39,10 +37,6 @@ app.get("/api/messages", async (req, res) => {
   }
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
-
 
 const PORT = process.env.PORT || 5000;
 
@@ -56,7 +50,7 @@ mongoose
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://encrypted-chatapp.vercel.app",
     methods: ["GET", "POST"],
   },
 });
